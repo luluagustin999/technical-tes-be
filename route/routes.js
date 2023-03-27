f90886da4b3463cfd60416ace9505a49410c5178
My Name <lulu.agustin105319@students.unila.ac.id>
2023-03-27 15:59:01 +0700

    add route delete

--- route/routes.js
+++ route/routes.js
@@ -10,6 +10,7 @@ router.get("/", (req, res) => {
 router.post("/mahasiswa/add", userController.createUser)
 router.get("/mahasiswa", userController.getUser)
 router.put("/mahasiswa/:id", userController.updateUser)
+router.put("/mahasiswa/:id", userController.deleteUser)
 
 module.exports = router;
 
